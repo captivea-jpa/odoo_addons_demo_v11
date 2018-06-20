e98363026b525be27a59aac87b152c9c1dc2f80a
DarkSark73 <youri.lebarch@captivea.fr>
2018-06-20 12:05:46 +0200

    maj

--- cap_demo/models/sale_config_setting.py
+++ cap_demo/models/sale_config_setting.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 import logging
+
 from odoo import api, fields, models
 
 _logger = logging.getLogger(__name__)
+
 
 class SaleConfiguration(models.TransientModel):
     _inherit = 'sale.config.settings'

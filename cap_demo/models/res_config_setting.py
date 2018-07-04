a052aa46bfc8e58447cf62e84cf1d4cf4c02e1c7
captivea-jpa <joffrey.paret@captivea.fr>
2018-07-04 10:30:10 +0200

    #15196

--- cap_demo/models/res_config_setting.py
+++ cap_demo/models/res_config_setting.py
@@ -6,39 +6,31 @@ from odoo import api, fields, models
 _logger = logging.getLogger(__name__)
 
 
-class SaleConfiguration(models.TransientModel):
+class ResConfiguration(models.TransientModel):
     _inherit = 'res.config.settings'
     
-    # Inherit field
+    # SALE
     group_product_variant = fields.Boolean(default=1)
     group_uom = fields.Boolean(default=1)
     multi_sales_price_method = fields.Selection(default='formula')
     group_sale_layout = fields.Boolean(default=1)
     group_route_so_lines = fields.Boolean(default=1)
     
-    group_discount_per_so_lines = fields.Boolean(default=1)
+    group_discount_per_so_line = fields.Boolean(default=1)
     portal_confirmation = fields.Boolean(default=1)
-    
+    portal_confirmation_options = fields.Selection(default='sign')
+    module_sale_payment = fields.Boolean(default=1)
 
-class StockSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    # Inherit field
-    group_stock_packaging = fields.Boolean(default=1)
-    
+    # STOCK
+    group_stock_packaging = fields.Boolean(default=1)    
     module_stock_barcode = fields.Boolean(default=1)
     module_stock_picking_batch = fields.Boolean(default=1)
 
-class InvoiceSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
-
-    # Inherit field
+    # INVOICE
     module_account_sepa = fields.Boolean(default=1)
     module_account_sepa_direct_debit = fields.Boolean(default=1)
     module_account_payment = fields.Boolean(default=1)
     
-class WebsiteSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
 
-    # Inherit field
+    # WEBSITE
     language_ids = fields.Many2many(default=lambda self: self.env['res.lang'].search([('code','=','fr_FR')]))
